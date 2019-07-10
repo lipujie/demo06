010df59e5837ab7948b31aa37298c698ef24f72d
tom_glb <xlcocon@email.com>
2019-07-10 11:31:53 +0800

    my first commit

---
+++ src/test/java/net/xdclass/demo3/Demo3ApplicationTests.java
@@ -0,0 +1,17 @@
+package net.xdclass.demo3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
